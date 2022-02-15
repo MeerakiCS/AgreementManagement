70477e9f4116f6ac70060ae18a0924ce6c3f5415
Shubham Rana <srana@mannacor.com>
2022-02-15 12:43:09 +0530

    Datatable

---
+++ AM.Web/AM.Web/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using AM.Web.Data;
+using AM.Web.Data.BusinessAccess;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AM.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserBusinessAccess _user;
+        public UserController(ApplicationDbContext context, IMapper mapper)
+        {
+            _user = new UserBusinessAccess(context, mapper);
+        }
+        // GET: UserController
+        public ActionResult Index()
+        {
+            var users = _user.GetAllUsers();
+            return View(users);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- AM.Web/AM.Web/Data/BusinessAccess/UserBusinessAccess.cs
+++ AM.Web/AM.Web/Data/BusinessAccess/UserBusinessAccess.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using AM.Web.Models;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AM.Web.Data.BusinessAccess
 {
+
     public class UserBusinessAccess
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public UserBusinessAccess(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        internal IEnumerable<UserModel> GetAllUsers()
+        {
+            var users = _context.ApplicationUser.ToList();
+            return _mapper.Map<IEnumerable<UserModel>>(users).ToList();
+        }
     }
 }
