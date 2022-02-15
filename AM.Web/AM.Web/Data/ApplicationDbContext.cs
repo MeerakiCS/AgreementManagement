f5628235d999cef5829f07a2c0a057d144fa5eb5
Shashi Bhushan <sbhushan@meerakics.com>
2022-02-15 10:27:16 +0530

    Initial setup

---
+++ AM.Web/AM.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AM.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AM.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
