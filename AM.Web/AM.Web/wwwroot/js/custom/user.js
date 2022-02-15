56933c622fca1d4d1d9569305062fd7dbce419e8
Shubham Rana <srana@mannacor.com>
2022-02-15 13:16:39 +0530

    Impliment datatable

---
+++ AM.Web/AM.Web/wwwroot/js/custom/user.js
@@ -0,0 +1,19 @@
+﻿var agreementDataTable;
+function initDatatable() {
+    agreementDataTable = $("#users-table").DataTable({
+        responsive: true,
+        bPaginate: true,
+        bFilter: true,
+        bInfo: true,
+        bLengthChange: false,
+        bAutoWidth: true,
+        pageLength: 25,
+        searching: true,
+        "ordering": true,
+        //'columnDefs': [{ 'orderable': false, 'targets': [5] }], // hide sort icon on header of first column
+        'aaSorting': [[0, 'desc']]
+    });
+}
+$(document).ready(function () {
+    initDatatable();
+})
